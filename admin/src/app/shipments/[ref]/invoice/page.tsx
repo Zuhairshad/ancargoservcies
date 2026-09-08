6c4749c881f5b911af4f7ee9bf77472f5a0e4cd9
Zuhair Shad <123764183+Zuhairshad@users.noreply.github.com>
2026-09-08 18:00:08 +0500

    Invoice: logo and INVOICE title side by side in same row

--- admin/src/app/shipments/[ref]/invoice/page.tsx
+++ admin/src/app/shipments/[ref]/invoice/page.tsx
@@ -58,11 +58,10 @@ export default async function InvoicePage({ params }: Params) {
           </div>
 
           <div className="inv">
-            <div className="inv__title">INVOICE</div>
-
-            <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '0.3rem 0 0.1rem' }}>
+            <div className="inv__title" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', textAlign: 'left' }}>
+              <span>INVOICE</span>
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src={`${MAIN_SITE}/images/logo.webp`} alt="AN Cargo Services" style={{ height: '60px', width: 'auto' }} />
+              <img src={`${MAIN_SITE}/images/logo.webp`} alt="AN Cargo Services" style={{ height: '60px', width: 'auto', display: 'block' }} />
             </div>
 
             <div className="inv__shipper">
